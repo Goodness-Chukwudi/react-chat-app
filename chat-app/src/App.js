31e7d27d8d600200a096525a9f5145c5d0adffa4
Goodness-Chukwudi <chukwudi.ibeche@moninetworks.com>
2022-09-27 06:05:38 +0100

    project setup

---
+++ chat-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatView from './features/chartView/chatView';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+        <ChatView />
+    </div>
+  );
+}
+
+export default App;
